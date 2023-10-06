75340550b80c9831861c38e2d121755b5d6ead60
Aquamoris <pestryak.nikita.dev@gmail.com>
2023-10-06 15:01:52 +1000

    [51] Made a request to the server and got users via axios get request

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,43 +1,17 @@
 import React from 'react';
 import style from './Users.module.css';
+import axios from "axios";
+import userPhoto from '../../assets/images/user.webp';
 
 const Users = (props) => {
 
     if (props.users.length === 0) {
-        props.setUsers([
-            {
-                id: 1,
-                photoUrl: 'https://cdn.popcake.tv/wp-content/uploads/2020/07/66623801_215088636120287_3722387074360083737_n.jpg',
-                followed: true,
-                fullName: 'Dmitry K',
-                status: 'I`m a boss',
-                location: {city: 'Minsk', country: 'Belarus'}
-            },
-            {
-                id: 2,
-                photoUrl: 'https://cdn.popcake.tv/wp-content/uploads/2020/07/66623801_215088636120287_3722387074360083737_n.jpg',
-                followed: false,
-                fullName: 'Sasha A',
-                status: 'I`m a boss too',
-                location: {city: 'Moscow', country: 'Russia'}
-            },
-            {
-                id: 3,
-                photoUrl: 'https://cdn.popcake.tv/wp-content/uploads/2020/07/66623801_215088636120287_3722387074360083737_n.jpg',
-                followed: false,
-                fullName: 'Sergey P',
-                status: 'I`m a boss too',
-                location: {city: 'Vladivostok', country: 'Russia'}
-            },
-            {
-                id: 4,
-                photoUrl: 'https://cdn.popcake.tv/wp-content/uploads/2020/07/66623801_215088636120287_3722387074360083737_n.jpg',
-                followed: true,
-                fullName: 'Andrew T',
-                status: 'I`m a boss too',
-                location: {city: 'Vladivostok', country: 'Russia'}
-            },
-        ])
+
+        axios
+            .get("https://social-network.samuraijs.com/api/1.0/users")
+            .then(response => {
+                props.setUsers(response.data.items);
+            });
     }
 
     return (
@@ -46,7 +20,7 @@ const Users = (props) => {
                 props.users.map(u => <div key={u.id}>
                     <span>
                         <div>
-                            <img className={style.userPhoto} src={u.photoUrl} alt="avatar"/>
+                            <img className={style.userPhoto} src={u.photos.small ? u.photos.small : userPhoto} alt="avatar"/>
                         </div>
                         <div>
                             { u.followed
@@ -56,12 +30,12 @@ const Users = (props) => {
                     </span>
                     <span>
                         <span>
-                            <div>{u.fullName}</div>
+                            <div>{u.name}</div>
                             <div>{u.status}</div>
                         </span>
                         <span>
-                            <div>{u.location.country}</div>
-                            <div>{u.location.city}</div>
+                            <div>{'u.location.country'}</div>
+                            <div>{'u.location.city'}</div>
                         </span>
                     </span>
                 </div>)

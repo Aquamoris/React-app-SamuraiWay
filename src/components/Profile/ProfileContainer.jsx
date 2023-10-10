c8b03ff991b29c7149757df21c880d9cfd9b5a4e
Aquamoris <pestryak.nikita.dev@gmail.com>
2023-10-10 23:30:10 +1000

    [60] Realization of withRouter HOC version +6

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -3,12 +3,21 @@ import Profile from './Profile';
 import axios from "axios";
 import {connect} from 'react-redux';
 import { setUsersProfile } from "../../redux/profileReducer";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 
 class ProfileContainer extends React.Component{
 
     componentDidMount() {
+
+        //get id from URL
+        let userId = this.props.router.params.userId;
+
+        if (!userId) {
+            userId = 2;
+        }
+
         axios
-            .get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
+            .get(`https://social-network.samuraijs.com/api/1.0/profile/` + userId)
             .then(response => {
                 this.props.setUsersProfile(response.data);
             });
@@ -27,4 +36,17 @@ const mapStateToProps = (state) => ({
     profile: state.profilePage.profile
 });
 
-export default connect(mapStateToProps, {setUsersProfile})(ProfileContainer);
+//HOC withRouter v +6
+function withRouter(Component) {
+    function ComponentWithRouterProps(props) {
+        let location = useLocation();
+        let navigate = useNavigate();
+        let params = useParams();
+
+        return <Component {...props} router={{location, navigate, params}} />
+    }
+
+    return ComponentWithRouterProps;
+}
+
+export default connect(mapStateToProps, {setUsersProfile})(withRouter(ProfileContainer));
